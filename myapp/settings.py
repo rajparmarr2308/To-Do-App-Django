d44f26b3fe26c6bdac1778f3eb7023adcb615ac4
Raj Parmar <58849801+rajparmarr2308@users.noreply.github.com>
2020-08-28 15:41:03 +0530

    remov

--- myapp/settings.py
+++ myapp/settings.py
@@ -25,11 +25,11 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = '0'
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['w3devtodoapp.herokuapp.com']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
